fad4375ccb04d2ab6e8cbbd24bf3706f48f7ddf8
Ed Addario <ed.addario@paytrix.io>
2024-02-11 23:00:29 +0000

    Add CompletableFutureExample

--- src/main/java/org/addario/ReactiveLoom.java
+++ src/main/java/org/addario/ReactiveLoom.java
@@ -5,13 +5,23 @@ import java.util.concurrent.ExecutionException;
 public class ReactiveLoom {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        var baseCase = new BaseCase().getName(1_000_000);
-        System.out.println("The most frequent baseCase name is " + baseCase);
+        var quantity = 1_000_000;
+        var batchSize = 10_000;
 
-        var threadExample = new ThreadExample().getName(1_000_000, 10_000);
-        System.out.println("The most frequent threadExample name is " + threadExample);
+        var baseCase = new BaseCase();
+        System.out.println("The most frequent baseCase name is " + baseCase.getName(quantity));
+        System.out.println("-----------------------------------------------------------------------------------------");
 
-        var callableExample = new CallableExample().getName(1_000_000, 10_000);
-        System.out.println("The most frequent callableExample name is " + callableExample);
+        var threadExample = new ThreadExample();
+        System.out.println("The most frequent threadExample name is " + threadExample.getName(quantity, batchSize));
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        var callableExample = new CallableExample();
+        System.out.println("The most frequent callableExample name is " + callableExample.getName(quantity, batchSize));
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        var completableFutureExample = new CompletableFutureExample();
+        System.out.println("The most frequent completableFutureExample name is " + completableFutureExample.getName(quantity, batchSize));
+        System.out.println("-----------------------------------------------------------------------------------------");
     }
 }
