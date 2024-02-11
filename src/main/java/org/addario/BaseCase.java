f71d82bdf10de07cf411e86257e33333de2e411e
Ed Addario <ed.addario@paytrix.io>
2024-02-11 22:23:44 +0000

    Add BaseCase class

---
+++ src/main/java/org/addario/BaseCase.java
@@ -0,0 +1,23 @@
+package org.addario;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseCase {
+    public String getName(int quantity) {
+        var namesList = new Util().getNames(quantity);
+
+        // Aggregate counts
+        Map<String, Long> counts = new HashMap<>();
+        for (String name : namesList) {
+            counts.compute(name, (n, c) -> c == null ? 1L : c + 1);
+        }
+
+        // Find the max count
+        return counts.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get()
+                .getKey();
+    }
+}
