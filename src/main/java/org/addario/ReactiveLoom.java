c6e8e2ec601d38f879ea75eff020d588afdd685e
Ed Addario <ed.addario@paytrix.io>
2024-02-11 23:33:58 +0000

    Add ReactiveExample

--- src/main/java/org/addario/ReactiveLoom.java
+++ src/main/java/org/addario/ReactiveLoom.java
@@ -5,23 +5,44 @@ import java.util.concurrent.ExecutionException;
 public class ReactiveLoom {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        var quantity = 1_000_000;
-        var batchSize = 10_000;
+        var quantity = 10_000_000;
+        var batchSize = 100_000;
+        var start = 0L;
+        var stop = 0L;
 
         var baseCase = new BaseCase();
+        start = System.currentTimeMillis();
         System.out.println("The most frequent baseCase name is " + baseCase.getName(quantity));
+        stop = System.currentTimeMillis();
+        System.out.println("Time: " + (stop - start) + "ms");
         System.out.println("-----------------------------------------------------------------------------------------");
 
         var threadExample = new ThreadExample();
+        start = System.currentTimeMillis();
         System.out.println("The most frequent threadExample name is " + threadExample.getName(quantity, batchSize));
+        stop = System.currentTimeMillis();
+        System.out.println("Time: " + (stop - start) + "ms");
         System.out.println("-----------------------------------------------------------------------------------------");
 
         var callableExample = new CallableExample();
+        start = System.currentTimeMillis();
         System.out.println("The most frequent callableExample name is " + callableExample.getName(quantity, batchSize));
+        stop = System.currentTimeMillis();
+        System.out.println("Time: " + (stop - start) + "ms");
         System.out.println("-----------------------------------------------------------------------------------------");
 
         var completableFutureExample = new CompletableFutureExample();
+        start = System.currentTimeMillis();
         System.out.println("The most frequent completableFutureExample name is " + completableFutureExample.getName(quantity, batchSize));
+        stop = System.currentTimeMillis();
+        System.out.println("Time: " + (stop - start) + "ms");
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        var reactiveExample = new ReactiveExample();
+        start = System.currentTimeMillis();
+        System.out.println("The most frequent reactiveExample name is " + reactiveExample.getName(quantity, batchSize));
+        stop = System.currentTimeMillis();
+        System.out.println("Time: " + (stop - start) + "ms");
         System.out.println("-----------------------------------------------------------------------------------------");
     }
 }
