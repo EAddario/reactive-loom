608f62098968ade62210d91b5b5f6e286ed0382f
Ed Addario <ed.addario@paytrix.io>
2024-03-16 12:49:12 +0000

    Add better program exit handling

--- src/main/java/org/addario/ReactiveLoom.java
+++ src/main/java/org/addario/ReactiveLoom.java
@@ -5,10 +5,10 @@ import java.util.UUID;
 
 public class ReactiveLoom {
     public static void main(String[] args) throws InterruptedException {
-        var quantity = 2_000_000; // 2 million payment records should take around 10 seconds to generate and will use about 3 GB of memory
+        var quantity = 2_000_000; // Generating 2 million payment records should take around 10 seconds and will use about 3.9 GB of memory
         var batchSize = 100_000; // quantity / batchSize = how many groups of payments to process in parallel
         var fileName = STR."\{UUID.randomUUID().toString()}.txt"; // Using Java 21 preview features so must be compiled/run with --enable-preview
-        File file = new File(fileName); // 2 million records will generate a file of around 3.3 GB
+        File file = new File(fileName); // A file with 2 million records will take around 3.3 GB
 
         var paymentsList = TimedExecution.createPaymentsList(quantity);
         TimedExecution.createPaymentsFile(fileName, paymentsList, file);
@@ -22,8 +22,11 @@ public class ReactiveLoom {
         TimedExecution.ioLoom(fileName, batchSize);
 
         if (file.exists())
-            file.delete();
+            if (file.delete())
+                System.exit(0); // Required to terminate Loom virtual threads
+            else
+                System.out.println(STR."Failed to delete file: \{fileName}");
 
-        System.exit(0); // Required to terminate Loom virtual threads
+        System.exit(1); // Something went wrong
     }
 }
