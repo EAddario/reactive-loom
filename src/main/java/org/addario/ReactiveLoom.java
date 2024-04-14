1bbdef2d0c0a7c48b917463576ea0e9ce40e6ec3
Ed Addario <ed.addario@paytrix.io>
2024-04-14 12:33:23 +0100

    Add ReactorDebugAgent and BlockHound

--- src/main/java/org/addario/ReactiveLoom.java
+++ src/main/java/org/addario/ReactiveLoom.java
@@ -1,5 +1,8 @@
 package org.addario;
 
+import reactor.tools.agent.ReactorDebugAgent;
+import reactor.blockhound.BlockHound;
+
 import java.io.File;
 import java.util.UUID;
 
@@ -7,9 +10,12 @@ public class ReactiveLoom {
     public static void main(String[] args) throws InterruptedException {
         var quantity = 2_000_000; // Generating 2 million payment records should take around 10 seconds and will use about 3.9 GB of memory
         var batchSize = 100_000; // quantity / batchSize = how many groups of payments to process in parallel
-        var fileName = STR."\{UUID.randomUUID().toString()}.txt"; // Using Java 21 preview features so must be compiled/run with --enable-preview
+        var fileName = STR."\{UUID.randomUUID().toString()}.txt"; // Using Java 22 preview features so must be compiled/run with --enable-preview
         var file = new File(fileName); // A file with 2 million records will take around 3.3 GB
         System.setProperty("reactor.schedulers.defaultBoundedElasticOnVirtualThreads", "true");
+
+        ReactorDebugAgent.init(); // Dev friendly reactor stack traces
+        BlockHound.install(); // Detect and throw on blocking calls from non-blocking threads
 
         var paymentsList = TimedExecution.createPaymentsList(quantity);
         TimedExecution.createPaymentsFile(fileName, paymentsList, file);
