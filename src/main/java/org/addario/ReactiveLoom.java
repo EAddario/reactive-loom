852eb3dbd94fd8a45cb492535969a709a8d848ea
Ed Addario <ed.addario@paytrix.io>
2024-05-19 23:11:31 +0100

    Minor changes

--- src/main/java/org/addario/ReactiveLoom.java
+++ src/main/java/org/addario/ReactiveLoom.java
@@ -1,6 +1,5 @@
 package org.addario;
 
-import reactor.blockhound.BlockHound;
 import reactor.tools.agent.ReactorDebugAgent;
 
 import java.io.File;
@@ -8,14 +7,14 @@ import java.util.UUID;
 
 public class ReactiveLoom {
     public static void main(String[] args) throws InterruptedException {
-        var quantity = 3_000_000; // Generating 3 million payment records should take around 20 seconds and will use about 3 GB of memory
+        var quantity = 3_000_000; // Generating 3 million payment records should take around 20 seconds and will use about 2.6 GB of memory
         var batchSize = 100_000; // quantity / batchSize = how many groups of payments to process in parallel
-        var fileName = STR."\{UUID.randomUUID().toString()}.txt"; // Using Java 21 preview features so must be compiled/run with --enable-preview
+        var fileName = STR."\{UUID.randomUUID().toString()}.txt"; // Using Java 22 preview features so must be compiled/run with --enable-preview
         var file = new File(fileName); // A file with 3 million records will take around 2.5 GB
         System.setProperty("reactor.schedulers.defaultBoundedElasticOnVirtualThreads", "true"); // Use virtual threads (project Loom) in bounded elastic scheduler
 
         ReactorDebugAgent.init(); // Dev friendly reactive stack traces
-        BlockHound.install(); // Detect and throw on blocking calls from non-blocking threads
+        //BlockHound.install(); // Detect and throw on blocking calls from non-blocking threads
 
         var start = System.currentTimeMillis();
         var paymentsList = TimedExecution.createPaymentsList(quantity);
